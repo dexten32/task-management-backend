e92315668e4806eab1bc074f45c493bcec8742cd
harsh <harshbajaj544@gmail.com>
2025-07-29 11:23:49 +0530

    CORS Error fix

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://task-management-backend-iyjp.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
